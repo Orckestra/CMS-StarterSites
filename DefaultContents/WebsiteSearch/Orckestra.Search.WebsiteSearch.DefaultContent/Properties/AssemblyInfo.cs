c663bb209d1d206ea2db097d1a6e3a2aae5675bd
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2017-06-08 14:47:02 +0200

    [Orckestra.Search.WebsiteSearch.DefaultContent] Initial commit

---
+++ DefaultContents/WebsiteSearch/Orckestra.Search.WebsiteSearch.DefaultContent/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Orckestra.Search.WebsiteSearch.DefaultContent")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Orckestra A/S")]
+[assembly: AssemblyProduct("Orckestra.Search.WebsiteSearch.DefaultContent")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c7b3fa0a-e53f-473f-8ec2-5553f0498dc4")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
